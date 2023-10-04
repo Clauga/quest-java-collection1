ea2f097cf4dbf0b6933ccb4be6d6a005651af742
clauga <guerrero.ayala.cs@gmail.com>
2023-10-04 20:36:54 +0200

    challenge done

--- Hero.java
+++ Hero.java
@@ -12,19 +12,23 @@ public class Hero {
 
     // accesseurs (getters)
     public String getName() {
-        return name;
-    }
 
-    public int getAge() {
-        return age;
+        return name;
     }
 
     // mutateurs (setters)
     public void setName(String name) {
+
         this.name = name;
     }
 
+    public int getAge() {
+
+        return age;
+    }
+
     public void setAge(int age) {
+
         this.age = age;
     }
 }
